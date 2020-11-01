ae61820575af065a7324289dd28391d1e24b2925
Joshua Meza <joshuaMeza2001@gmail.com>
2020-11-01 02:16:22 -0600

    Rework and clean

--- js/script.js
+++ js/script.js
@@ -1,31 +1,15 @@
-// Load
-window.onload = function(){
-    $('#onload').fadeOut();
-    $('body').removeClass('hidden');
-}
-
-// Carousel Hero
+// Pausing the start of the carousel
 $('.carousel').carousel({
-    interval: 4000
+    ride: true,
 })
 
-// Changes
-if ( screen.width < 1025 ){
-    document.getElementById('hero1').src = './img/heroOneMobile.png';
-    document.getElementById('hero2').src = './img/heroTwoMobile.png';
-    document.getElementById('hero3').src = './img/heroThreeMobile.png';
-    document.getElementById('hero4').src = './img/heroFourMobile.png';
-}else{
-    document.getElementById('change').innerHTML = 'En la sección de "Mascotas" podrás conocer un poco acerca de los Bulldog Ingleses, Yorkshire Terriers y Shih Tzus.';
-}
+// Loading screen
+$(document).ready(() => {
+    $('#onload').fadeOut();
 
-//Carousel slick
-$('.productSlick').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 1,
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // Activating the carousel
+    $('.carousel').carousel({
+        interval: 500,
+        ride: false,
+    })
 })
