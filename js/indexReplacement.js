c828f3ac1010b2583945efbf44063dcc665c1397
Joshua Meza <joshuaMeza2001@gmail.com>
2020-11-15 02:24:27 -0600

    Indentation

--- js/indexReplacement.js
+++ js/indexReplacement.js
@@ -1,7 +1,9 @@
 /* Index Hero */
-if ( screen.width <= 575 ){
-    document.getElementById('indexHeroOne').src='./img/index/heroOneMobile.png';
-    document.getElementById('indexHeroTwo').src='./img/index/heroTwoMobile.png';
-    document.getElementById('indexHeroThree').src='./img/index/heroThreeMobile.png';
-    document.getElementById('indexHeroFour').src='./img/index/heroFourMobile.png';
-};
+if (screen.width <= 575) {
+  document.getElementById("indexHeroOne").src = "./img/index/heroOneMobile.png";
+  document.getElementById("indexHeroTwo").src = "./img/index/heroTwoMobile.png";
+  document.getElementById("indexHeroThree").src =
+    "./img/index/heroThreeMobile.png";
+  document.getElementById("indexHeroFour").src =
+    "./img/index/heroFourMobile.png";
+}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Pausing the start of the carousel
-$('.carousel').carousel({
-    ride: true,
-})
+$(".carousel").carousel({
+  ride: true,
+});
 
 // Loading screen
-$(window).on('load', function () {
-    $('#onload').fadeOut('slow');
+$(window).on("load", function () {
+  $("#onload").fadeOut("slow");
 
-    // Activating the carousel
-    $('.carousel').carousel({
-        interval: 500,
-        ride: false,
-    })
-})
+  // Activating the carousel
+  $(".carousel").carousel({
+    interval: 500,
+    ride: false,
+  });
+});
